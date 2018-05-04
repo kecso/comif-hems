b1a0d2052a276b8713c60197ad688514976cdb60
Tamas Kecskes <tamas.kecskes@vanderbilt.edu>
2018-05-04 12:15:40 -0500

    Strated gremlin translation

--- src/plugins/ToGremlin/ToGremlin.js
+++ src/plugins/ToGremlin/ToGremlin.js
@@ -10,11 +10,21 @@
 define([
     'plugin/PluginConfig',
     'text!./metadata.json',
-    'plugin/PluginBase'
+    'plugin/PluginBase',
+    'ejs',
+    'text!./queries.txt',
+    'text!./meta.ejs',
+    'text!./node.ejs',
+    'text!./values.ejs'
 ], function (
     PluginConfig,
     pluginMetadata,
-    PluginBase) {
+    PluginBase,
+    ejs,
+    QUERIES,
+    metaTemplate,
+    nodeTemplate,
+    valueTemplate) {
     'use strict';
 
     pluginMetadata = JSON.parse(pluginMetadata);
@@ -56,35 +66,146 @@ define([
         // Use self to access core, project, result, logger etc from PluginBase.
         // These are all instantiated at this point.
         var self = this,
-            nodeObject;
+            concept,
+            conceptObject,
+            nodeObject,
+            keys, i, j, k, target, meta, owner,
+            p2c = {},
+            p2n = {},
+            parameters = {},
+            gremlin = '';
 
-
-        // Using the logger.
-        self.logger.debug('This is a debug message.');
-        self.logger.info('This is an info message.');
-        self.logger.warn('This is a warning message.');
-        self.logger.error('This is an error message.');
-
-        // Using the coreAPI to make changes.
 
         nodeObject = self.activeNode;
 
-        self.core.setAttribute(nodeObject, 'name', 'My new obj');
-        self.core.setRegistry(nodeObject, 'position', {x: 70, y: 70});
+        for (concept in self.META) {
+            p2c[self.core.getPath(self.META[concept])] = concept;
+            p2n[self.core.getPath(self.META[concept])] = self.META[concept];
+        }
 
+        parameters.name = self.core.getAttribute(nodeObject, 'name');
+        parameters.concepts = [];
+        for (concept in self.META) {
+            conceptObject = {
+                name: concept,
+                attr: {},
+                ptr: {},
+                set: {},
+                bases: []
+            };
 
-        // This will save the changes. If you don't want to save;
-        // exclude self.save and call callback directly from this scope.
-        self.save('ToGremlin updated model.')
-            .then(function () {
-                self.result.setSuccess(true);
-                callback(null, self.result);
-            })
-            .catch(function (err) {
-                // Result success is false at invocation.
+            keys = self.core.getOwnValidAttributeNames(self.META[concept]);
+            for (i = 0; i < keys.length; i += 1) {
+                conceptObject.attr[keys[i]] = self.core.getAttributeMeta(self.META[concept], keys[i]).type || 'string';
+            }
+
+            keys = self.core.getOwnValidPointerNames(self.META[concept]);
+            for (i = 0; i < keys.length; i += 1) {
+                //TODO merge all together for better quality
+                conceptObject.ptr[keys[i]] = p2c[self.core.getOwnValidTargetPaths(self.META[concept], keys[i])[0]];
+                if (!conceptObject.ptr[keys[i]]) {
+                    delete conceptObject.ptr[keys[i]];
+                }
+            }
+
+            keys = self.core.getOwnValidSetNames(self.META[concept]);
+            for (i = 0; i < keys.length; i += 1) {
+                target = p2c[self.core.getOwnValidTargetPaths(self.META[concept], keys[i])[0]];
+                if (target) {
+                    meta = self.core.getPointerMeta(self.META[concept], keys[i]);
+                    for (j in meta) {
+                        if (p2c[j] == target) {
+                            conceptObject.set[keys[i]] = {
+                                target: target,
+                                md: {min: meta[j].min, max: meta[j].max}
+                            }
+                        }
+                    }
+                }
+            }
+
+            keys = self.core.getMixinPaths(self.META[concept]);
+            for (i = 0; i < keys.length; i += 1) {
+                conceptObject.bases.push(p2c[keys[i]])
+            }
+            if (self.core.getBase(self.META[concept])) {
+                conceptObject.bases.push(p2c[self.core.getPath(self.core.getBase(self.META[concept]))]);
+            }
+            parameters.concepts.push(conceptObject);
+        }
+
+        gremlin += ejs.render(metaTemplate, parameters);
+        self.core.loadSubTree(nodeObject, function (err, nodes) {
+            var values = {string: [], integer: [], float: [], asset: []};
+            if (err) {
+                self.result.setSuccess(false);
                 callback(err, self.result);
-            });
+                return;
+            }
 
+            p2n[self.core.getPath(nodeObject)] = nodeObject;
+            for (i = 0; i < nodes.length; i += 1) {
+                p2n[self.core.getPath(nodes[i])] = nodes[i];
+            }
+
+            for (i = 0; i < nodes.length; i += 1) {
+                parameters = {attr: {}, ptr: {}, set: {}};
+                parameters.path = self.core.getPath(nodes[i]);
+                parameters.metaType = p2c[self.core.getPath(self.core.getMetaType(nodes[i]))];
+                //attr
+                keys = self.core.getValidAttributeNames(nodes[i]);
+                for (j = 0; j < keys.length; j += 1) {
+                    owner = self.core.getAttributeDefinitionOwner(nodes[i], keys[j]);
+                    meta = self.core.getAttributeMeta(owner, keys[j]).type || 'string';
+                    target = '' + self.core.getAttribute(nodes[i], keys[j]);
+                    k = values[meta].indexOf(target);
+                    if (k === -1) {
+                        k = values[meta].length;
+                        values[meta].push(target);
+                    }
+                    parameters.attr[keys[j]] = {
+                        owner: self.core.getAttribute(owner, 'name'),
+                        type: meta,
+                        valueIndex: k
+                    };
+
+
+                }
+                //ptr
+                keys = self.core.getValidPointerNames(nodes[i]);
+                for (j = 0; j < keys.length; j += 1) {
+                    target = p2n[self.core.getPointerPath(nodes[i], keys[j])];
+                    if (target) {
+                        owner = self.core.getPointerDefinitionInfo(nodes[i], keys[j], target);
+                        parameters.ptr[keys[j]] = {
+                            owner: p2c[owner.ownerPath],
+                            target: self.core.getPointerPath(nodes[i], keys[j])
+                        };
+                    }
+                }
+                //set
+                keys = self.core.getValidSetNames(nodes[i]);
+                for (j = 0; j < keys.length; j += 1) {
+                    parameters.set[keys[j]] = {};
+                    target = self.core.getMemberPaths(nodes[i], keys[j]);
+                    for (k = 0; k < target.length; k += 1) {
+                        owner = self.core.getSetDefinitionInfo(nodes[i], keys[j], target[k]);
+                        parameters.set[keys[j]][target[k]] = p2c[owner.ownerPath];
+                    }
+                }
+
+                parameters.values = values;
+                gremlin += ejs.render(nodeTemplate, parameters);
+            }
+
+
+            gremlin += ejs.render(valueTemplate, {values: values});
+
+            gremlin +='\n}\n';
+            console.log(gremlin);
+            self.result.setSuccess(true);
+            callback(null, self.result);
+        });
     };
 
     return ToGremlin;
